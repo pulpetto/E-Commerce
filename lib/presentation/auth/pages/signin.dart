bbb73dc7a4eef5cf3d39a9249db738758e17f63c
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-13 05:58:49 +0200

    add sign-in page

---
+++ lib/presentation/auth/pages/signin.dart
@@ -0,0 +1,70 @@
+import 'package:e_commerce/common/widgets/buttons/basic_app_button.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class SigninPage extends StatelessWidget {
+  const SigninPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _signinText(context),
+            const SizedBox(height: 20),
+            _emailField(context),
+            const SizedBox(height: 20),
+            _continueButton(context),
+            const SizedBox(height: 20),
+            _createAccount(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return const Text(
+      'Sign in',
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(hintText: 'Enter e-mail'),
+    );
+  }
+
+  Widget _continueButton(BuildContext context) {
+    return BasicAppButton(
+      onPressed: () {},
+      title: 'Continue',
+    );
+  }
+
+  Widget _createAccount(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          const TextSpan(
+            text: 'Don\'t you have an account? ',
+          ),
+          TextSpan(
+            text: 'Create one',
+            recognizer: TapGestureRecognizer()..onTap = () {},
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

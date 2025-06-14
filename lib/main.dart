a817d7da9a17f9441951f4c510334cfff495e373
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-14 06:44:02 +0200

    add & configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:e_commerce/core/config/theme/app_theme.dart';
+import 'package:e_commerce/firebase_options.dart';
 import 'package:e_commerce/presentation/splash/bloc/splash_qubit.dart';
 import 'package:e_commerce/presentation/splash/pages/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
